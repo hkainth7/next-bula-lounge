f05d491e95b5061ab35a87558bc3b382b68d911c
Harjit Kainth <harjit.kainth7@gmail.com>
2024-08-04 10:12:09 -0700

    change styles

--- app/_components/CarouselCard/index.tsx
+++ app/_components/CarouselCard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardHeader, Typography } from "@mui/material";
 import { CarouselCardProps } from "./types";
 import Image from "next/image";
 
@@ -7,21 +7,27 @@ const CarouselCard = (props: CarouselCardProps) => {
 	const { title, src, description } = props;
 
 	return (
-		<Card
-			sx={{ backgroundColor: "#222831", borderRadius: "4px", padding: "4px" }}
-		>
+		<Card sx={{ backgroundColor: "#222831", borderRadius: "4px" }}>
 			<CardHeader
-				sx={{ color: "white" }}
+				sx={{ color: "white", padding: "8px" }}
 				title={title}
 				titleTypographyProps={{ variant: "h6" }}
 			/>
 			<CardContent
-				sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+					padding: "8px",
+				}}
 			>
-				<Image src={src} height={300} width={300} alt={`${title} hookah`} />
-				<Typography variant="body2" color={"white"}>
-					{description}
-				</Typography>
+				<Image src={src} height={300} width={0} alt={`${title} hookah`} />
+				<Box sx={{ padding: "0px 14px" }}>
+					<Typography variant="body2" color={"white"}>
+						{description}
+					</Typography>
+				</Box>
 			</CardContent>
 		</Card>
 	);
