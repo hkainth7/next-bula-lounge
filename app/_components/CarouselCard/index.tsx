20662749d26998a1dd180317a243f05e41e486ef
Harjit Kainth <harjit.kainth7@gmail.com>
2024-08-04 09:27:13 -0700

    changed some styles, added footer styling

--- app/_components/CarouselCard/index.tsx
+++ app/_components/CarouselCard/index.tsx
@@ -7,10 +7,16 @@ const CarouselCard = (props: CarouselCardProps) => {
 	const { title, src, description } = props;
 
 	return (
-		<Card sx={{ width: "100%", backgroundColor: "black" }}>
-			<CardHeader title={title} sx={{ color: "white" }} />
-			<CardContent>
-				<Image src={src} height={200} width={200} alt={`${title} hookah`} />
+		<Card
+			sx={{ backgroundColor: "#222831", borderRadius: "4px", padding: "4px" }}
+		>
+			<CardHeader
+				sx={{ color: "white" }}
+				title={title}
+				titleTypographyProps={{ variant: "h6" }}
+			/>
+			<CardContent sx={{ display: "flex", justifyContent: "center" }}>
+				<Image src={src} height={300} width={300} alt={`${title} hookah`} />
 				<Typography variant="body2" color={"white"}>
 					{description}
 				</Typography>
