65b7237f57af2a510563313bafcf4030934a64f8
Harjit Kainth <harjit.kainth7@gmail.com>
2024-08-06 09:51:17 -0700

    added new styling, added content on main page

--- app/_components/Homepage/index.tsx
+++ app/_components/Homepage/index.tsx
@@ -2,11 +2,37 @@ import React from "react";
 
 import { EmblaOptionsType } from "embla-carousel";
 import EmblaCarousel from "../EmblaCarousel";
+import { Stack, Typography } from "@mui/material";
 
 const OPTIONS: EmblaOptionsType = { loop: true };
 
 const Homepage = () => {
-	return <EmblaCarousel options={OPTIONS} />;
+	return (
+		<Stack>
+			<Typography
+				variant="h1"
+				sx={{
+					fontSize: "40px",
+					marginY: "10px",
+					fontWeight: "bold",
+					padding: "4px",
+					textAlign: "center",
+				}}
+			>
+				Welcome to <span style={{ color: "green" }}>Bula Hookah</span> Lounge!
+			</Typography>
+			<Typography
+				variant="h6"
+				textAlign={"center"}
+				sx={{ marginY: "20px", fontWeight: "light" }}
+			>
+				Discover the finest hookah flavors in our cozy, inviting space with
+				plush seating and ambient lighting. Perfect for catching up with friends
+				or relaxing after a long day
+			</Typography>
+			<EmblaCarousel options={OPTIONS} />
+		</Stack>
+	);
 };
 
 export default Homepage;
