dfb760a1f20047850d612314f6ea56c1064a89e6
Harjit Kainth <harjit.kainth7@gmail.com>
2024-08-05 21:31:35 -0700

    add socials and icons to footer

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,27 +1,30 @@
-import { Box, Divider, Stack, Typography } from "@mui/material";
+import { Divider, Stack, Typography } from "@mui/material";
 import Link from "next/link";
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import { TikTokIcon } from "./TikTokIcon";
 
 const Footer = () => {
 	return (
 		<footer>
-			<Stack direction="row" alignItems="center" justifyContent="space-between">
+			<Stack direction="row" alignItems="center">
 				<Link href="https://www.instagram.com/bulahookahlounge?igsh=MTMzengydnk2MXFiNw==">
 					<InstagramIcon fontSize="large" />
 				</Link>
-				<Stack alignItems="flex-start">
-					<Stack direction="row" alignItems="center">
-						<LocationOnIcon fontSize="small" sx={{ marginRight: "8px" }} />
-						<Typography>8027 Hastings St, Burnaby</Typography>
-					</Stack>
-					<Stack direction="row" alignItems="center">
-						<LocalPhoneIcon fontSize="small" sx={{ marginRight: "8px" }} />
-						<Typography>236 558-3099</Typography>
-					</Stack>
-				</Stack>
+				<Link href="https://youtube.com/@bulahookahlounge?si=-PkGPD7CmQ_NOf4a">
+					<YouTubeIcon fontSize="large" />
+				</Link>
+				<Link href="https://www.facebook.com/share/AUrtqQFX3K6hvjua/?mibextid=qi2Omg">
+					<FacebookIcon fontSize="large" />
+				</Link>
+				<Link
+					href="https://youtube.com/@bulahookahlounge?si=-PkGPD7CmQ_NOf4a"
+					style={{ width: "35px" }}
+				>
+					<TikTokIcon color={"white"} />
+				</Link>
 			</Stack>
 			<Divider sx={{ marginY: "5px" }} />
 			Powered by<Link href={""}>HKDev</Link>
